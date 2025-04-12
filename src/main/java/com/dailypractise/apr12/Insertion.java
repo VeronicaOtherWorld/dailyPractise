fb95707a90bb6638d7f867c094cd7fc95d011b61
VeronicaOtherWorld <aisileyi@gmail.com>
2025-04-12 22:08:11 +0100

    update

---
+++ src/main/java/com/dailypractise/apr12/Insertion.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dailypractise.apr12;
+
+/**
+ *
+ * @author luyi
+ */
+public class Insertion {
+    public static void insertion(int[] f){
+       int s = 1;
+       while(s < f.length){
+           int current = s;
+           while(current > 0 && f[current] < f[current -1]){
+               int temp = f[current];
+               f[current] = f[current -1];
+               f[current -1] = temp;
+               current--;
+           }
+           s++;
+       }
+    }
+}
---
+++ src/main/java/com/dailypractise/apr12/MergeSort.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dailypractise.apr12;
+
+/**
+ *
+ * @author luyi
+ */
+public class MergeSort {
+    public static void mergeSort(int[] f, int l, int u){
+        if(l + 1 < u){
+            int mid = (l + u) /2;
+            mergeSort(f, l ,mid);
+            mergeSort(f, mid, u);
+            merge(f, l ,mid ,u);
+        }
+    }
+    public static void merge(int[] f, int l, int mid, int u) {
+        int i = l;
+        int j = mid;
+        int t = 0;
+        int temp[] = new int[u - l];
+        while (i < mid && j < u) {
+            if (f[i] < f[j]) {
+                temp[t] = f[i];
+                i++;
+                t++;
+            } else {
+                temp[t] = f[j];
+                j++;
+                t++;
+            }
+        }
+        while (i < mid) {
+            temp[t] = f[i];
+            i++;
+            t++;
+        }
+        while (j < u) {
+            temp[t] = f[j];
+            j++;
+            t++;
+        }
+
+        i = l;
+        t = 0;
+        while (t < temp.length) {
+            f[i] = temp[t];
+            i++;
+            t++;
+        }
+    }
+}
